3e347593471410a940ac0af26556f94314e95604
talipovm <talipovm@nmsu.edu>
2018-07-24 21:18:44 -0400

    moved GAMESS basis set processing in a separate function

--- terse/Interface/Gamess.py
+++ terse/Interface/Gamess.py
@@ -53,11 +53,17 @@ class Gamess(Top):
         self.P.postprocess()
 
         # GAMESS-specific edits
+        if self.P.parsed.last_value('gbasis') is not None:
+            self.postprocess_basis()
+
+        log.debug('%s postprocessed successfully' % self.file)
+
+    def postprocess_basis(self):
 
         P = self.P.parsed
         basis_words = ('gbasis', 'igauss', 'polar', 'ndfunc', 'nffunc', 'diffsp', 'npfunc', 'diffs')
         vals = list(P.last_value(s) if P.last_value(s) is not None else '' for s in basis_words)
-        b = dict(zip(basis_words,vals))
+        b = dict(zip(basis_words, vals))
 
         def polarf(plr, suffix):
             if plr in ('0', ''):
@@ -66,35 +72,32 @@ class Gamess(Top):
                 return suffix
             return plr + suffix
 
-        if b['gbasis']:
-            if b['gbasis'] in ('N21','N31','N311'):
-                basis = '%s-%s' % (b['igauss'],b['gbasis'][1:])
-                if 'T' in b['diffsp']:
+        if b['gbasis'] in ('N21', 'N31', 'N311'):
+            basis = '%s-%s' % (b['igauss'], b['gbasis'][1:])
+            if 'T' in b['diffsp']:
+                basis += '+'
+                if 'T' in b['diffs']:
                     basis += '+'
-                    if 'T' in b['diffs']:
-                        basis += '+'
-                basis += 'G'
+            basis += 'G'
 
-                polar_heavy = polarf(b['ndfunc'],'d') + polarf(b['nffunc'],'f')
-                if polar_heavy:
-                    polar_H = polarf(b['npfunc'], 'p')
-                    if polar_H:
-                        polar = '(%s,%s)' % (polar_heavy,polar_H)
-                    else:
-                        polar = '(%s)' % polar_heavy
+            polar_heavy = polarf(b['ndfunc'], 'd') + polarf(b['nffunc'], 'f')
+            if polar_heavy:
+                polar_H = polarf(b['npfunc'], 'p')
+                if polar_H:
+                    polar = '(%s,%s)' % (polar_heavy, polar_H)
                 else:
-                    polar = ''
+                    polar = '(%s)' % polar_heavy
+            else:
+                polar = ''
 
-                basis += polar
-                self.G_parsed.conditionally_add(old_key='P_basis',new_key='P_basis',new_value=basis)
+            basis += polar
+            self.G_parsed.conditionally_add(old_key='P_basis', new_key='P_basis', new_value=basis)
 
-            if b['gbasis']=='STO':
-                basis = 'STO-%sG' % b['igauss']
-                if b['ndfunc']!='0':
-                    basis += '*'
-                self.G_parsed.conditionally_add(old_key='P_basis',new_key='P_basis',new_value=basis)
-
-        log.debug('%s postprocessed successfully' % (self.file))
+        if b['gbasis'] == 'STO':
+            basis = 'STO-%sG' % b['igauss']
+            if b['ndfunc'] != '0':
+                basis += '*'
+            self.G_parsed.conditionally_add(old_key='P_basis', new_key='P_basis', new_value=basis)
 
     def webdata(self):
         """
